92488e6d034905938130ff677385b8e2cea1d905
Eric Wätke <email@ericwaetke.com>
2025-12-22 19:51:48 +0100

    chore: redit navigation and added video "search"

--- App/PeerTubeSwift/PeerTubeSwift/Views/VideoDetails.swift
+++ App/PeerTubeSwift/PeerTubeSwift/Views/VideoDetails.swift
@@ -11,7 +11,7 @@ import TubeSDK
 struct VideoDetails: View {
     @Environment(AppState.self) private var appState: AppState
     let host: String
-    let video: Video
+    let videoId: String
     
     @State var videoDetails: TubeSDK.VideoDetails?
     
@@ -19,96 +19,100 @@ struct VideoDetails: View {
     @State var hasDisliked = false
     
     var body: some View {
-        VStack {
-            if let videoDetails = videoDetails,
-               let streamingPlaylists = videoDetails.streamingPlaylists,
-               let video = streamingPlaylists.first,
-               let urlString = video.playlistUrl,
-               let url = URL(string: urlString){
-                VideoPlayerView(videoURL: url)
-                    .frame(
-                        minWidth: 0,
-                        maxWidth: .infinity,
-                        minHeight: 100,
-                        maxHeight: .infinity
-                    )
-                    .aspectRatio(16 / 9, contentMode: .fit)
-                    .padding(24)
-            }
-            VStack (alignment: .leading) {
-                VStack (alignment: .leading) {
-                    Text(video.name ?? "Unknown Video Title")
-                        .fontWeight(.bold)
-                    
-                    if let views = video.views {
-                        Text("^[\(views) View](inflect: true)")
-                            .font(.caption)
+        ZStack {
+            if let videoDetails = videoDetails {
+                VStack {
+                    if let streamingPlaylists = videoDetails.streamingPlaylists,
+                       let video = streamingPlaylists.first,
+                       let urlString = video.playlistUrl,
+                       let url = URL(string: urlString){
+                        VideoPlayerView(videoURL: url)
+                            .frame(
+                                minWidth: 0,
+                                maxWidth: .infinity,
+                                minHeight: 100,
+                                maxHeight: .infinity
+                            )
+                            .aspectRatio(16 / 9, contentMode: .fit)
+                            .padding(24)
                     }
-                    if let likes = video.likes,
-                       let dislikes = video.dislikes{
-                        HStack {
-                            Button {
-                                print("added like")
-                                hasLiked = true
-                                hasDisliked = false
-                            } label: {
-                                HStack {
-                                    Image(systemName: "hand.thumbsup")
-                                    Text(likes.formatted())
-                                }
-                            }
-                            .tint(hasLiked ? .blue : .primary)
-                            .apply {
-                                if #available(iOS 26.0, *) {
-                                    $0.buttonStyle(.glass)
-                                } else {
-                                    $0.buttonStyle(.automatic)
-                                }
-                            }
+                    VStack (alignment: .leading) {
+                        VStack (alignment: .leading) {
+                            Text(videoDetails.name ?? "Unknown Video Title")
+                                .fontWeight(.bold)
                             
-                            
-                            Button {
-                                print("added dislike")
-                                hasLiked = false
-                                hasDisliked = true
-                            } label: {
-                                HStack {
-                                    Image(systemName: "hand.thumbsdown")
-                                    Text(dislikes.formatted())
-                                }
+                            if let views = videoDetails.views {
+                                Text("^[\(views) View](inflect: true)")
+                                    .font(.caption)
                             }
-                            .tint(hasLiked ? .blue : .primary)
-                            .apply {
-                                if #available(iOS 26.0, *) {
-                                    $0.buttonStyle(.glass)
-                                } else {
-                                    $0.buttonStyle(.automatic)
+                            if let likes = videoDetails.likes,
+                               let dislikes = videoDetails.dislikes{
+                                HStack {
+                                    Button {
+                                        print("added like")
+                                        hasLiked = true
+                                        hasDisliked = false
+                                    } label: {
+                                        HStack {
+                                            Image(systemName: "hand.thumbsup")
+                                            Text(likes.formatted())
+                                        }
+                                    }
+                                    .tint(hasLiked ? .blue : .primary)
+                                    .apply {
+                                        if #available(iOS 26.0, *) {
+                                            $0.buttonStyle(.glass)
+                                        } else {
+                                            $0.buttonStyle(.automatic)
+                                        }
+                                    }
+                                    
+                                    
+                                    Button {
+                                        print("added dislike")
+                                        hasLiked = false
+                                        hasDisliked = true
+                                    } label: {
+                                        HStack {
+                                            Image(systemName: "hand.thumbsdown")
+                                            Text(dislikes.formatted())
+                                        }
+                                    }
+                                    .tint(hasLiked ? .blue : .primary)
+                                    .apply {
+                                        if #available(iOS 26.0, *) {
+                                            $0.buttonStyle(.glass)
+                                        } else {
+                                            $0.buttonStyle(.automatic)
+                                        }
+                                    }
                                 }
                             }
                         }
+                        .containerRelativeFrame(.horizontal)
+                        
+                        VStack {
+                            if let commentCount = videoDetails.comments {
+                                Text("^[\(commentCount) Comments](inflect: true)")
+                                    .fontWeight(.bold)
+                            }
+                        }
+                        Spacer()
                     }
+                    .padding()
+                    
+                    .containerRelativeFrame(.horizontal)
                 }
-                .containerRelativeFrame(.horizontal)
-                
-                VStack {
-                    if let commentCount = video.comments {
-                        Text("^[\(commentCount) Comments](inflect: true)")
-                            .fontWeight(.bold)
-                    }
-                }
-                Spacer()
             }
-            .padding()
-            
-            .containerRelativeFrame(.horizontal)
+            else {
+                ProgressView()
+            }
         }
-        .navigationTitle(video.name ?? "Unknown Video")
+        .navigationTitle(videoDetails?.name ?? "Unknown Video")
         .onAppear {
-            videoDetails = nil
+            if (videoDetails != nil) {return}
             Task {
-                if let uuid = video.uuid {
-                    videoDetails = try await appState.client.getVideo(host: host, id: uuid.uuidString)
-                }
+                videoDetails = try await appState.client.getVideo(host: host, id: videoId)
             }
         }
     }
