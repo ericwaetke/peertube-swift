250d976cbccc5a2be31b84d94248edc443b59a66
Eric Wätke <email@ericwaetke.com>
2025-12-27 22:32:09 +0100

    fix: TCA Stack Implementation for Explore Page

--- App/PeerTubeSwift/PeerTubeSwift/Tabs/Stack.swift
+++ App/PeerTubeSwift/PeerTubeSwift/Tabs/Stack.swift
@@ -114,74 +114,9 @@ struct NavigationDemoView: View {
         ScreenCView(store: store)
       }
     }
-    .safeAreaInset(edge: .bottom) {
-      FloatingMenuView(store: store)
-    }
     .navigationTitle("Navigation Stack")
   }
 }
-
-// MARK: - Floating menu
-
-struct FloatingMenuView: View {
-  let store: StoreOf<NavigationDemo>
-
-  struct ViewState: Equatable {
-    struct Screen: Equatable, Identifiable {
-      let id: StackElementID
-      let name: String
-    }
-
-    var currentStack: [Screen]
-    var total: Int
-    init(state: NavigationDemo.State) {
-      self.total = 0
-      self.currentStack = []
-      for (id, element) in zip(state.path.ids, state.path) {
-        switch element {
-        case let .screenA(screenAState):
-          self.total += screenAState.count
-          self.currentStack.insert(Screen(id: id, name: "Screen A"), at: 0)
-        case .screenB:
-          self.currentStack.insert(Screen(id: id, name: "Screen B"), at: 0)
-        case let .screenC(screenBState):
-          self.total += screenBState.count
-          self.currentStack.insert(Screen(id: id, name: "Screen C"), at: 0)
-        }
-      }
-    }
-  }
-
-  var body: some View {
-    let viewState = ViewState(state: store.state)
-    if viewState.currentStack.count > 0 {
-      VStack(alignment: .center) {
-        Text("Total count: \(viewState.total)")
-        Button("Pop to root") {
-          store.send(.popToRoot, animation: .default)
-        }
-        Menu("Current stack") {
-          ForEach(viewState.currentStack) { screen in
-            Button("\(String(describing: screen.id))) \(screen.name)") {
-              store.send(.goBackToScreen(id: screen.id))
-            }
-            .disabled(screen == viewState.currentStack.first)
-          }
-          Button("Root") {
-            store.send(.popToRoot, animation: .default)
-          }
-        }
-      }
-      .padding()
-      .background(Color(.systemBackground))
-      .padding(.bottom, 1)
-      .transition(.opacity.animation(.default))
-      .clipped()
-      .shadow(color: .black.opacity(0.2), radius: 5, y: 5)
-    }
-  }
-}
-
 // MARK: - Screen A
 
 @Reducer
