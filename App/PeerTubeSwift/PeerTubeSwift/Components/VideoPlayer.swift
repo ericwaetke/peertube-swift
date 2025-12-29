fb15dc5c4d76f9cee2690dd3ea62fa3868a18fd7
Eric Wätke <email@ericwaetke.com>
2025-12-29 11:42:48 +0100

    feat: Tablet Optimization

--- App/PeerTubeSwift/PeerTubeSwift/Components/VideoPlayer.swift
+++ App/PeerTubeSwift/PeerTubeSwift/Components/VideoPlayer.swift
@@ -10,19 +10,6 @@ import SwiftUI
 
 struct VideoPlayerView: UIViewControllerRepresentable {
     let videoURL: URL
-    let sidecarAudioUrl: URL?
-    
-    init(videoURL: URL, sidecarAudioUrl: URL?) {
-        if (sidecarAudioUrl != nil) {
-            print("Sidecar Audio Provided")
-        } else {
-            print("no sidecar audio provided")
-        }
-        self.videoURL = videoURL
-        self.sidecarAudioUrl = sidecarAudioUrl
-    }
-    
-    let coordinationMedium = AVPlaybackCoordinationMedium()
 
     func makeUIViewController(context: Context) -> AVPlayerViewController {
         let session = AVAudioSession.sharedInstance()
@@ -30,34 +17,6 @@ struct VideoPlayerView: UIViewControllerRepresentable {
         try? session.setActive(true)
 
         let player = AVPlayer(url: videoURL)
-        
-        do {
-            if #available(iOS 26.0, *) {
-                try player.playbackCoordinator.coordinate(using: coordinationMedium)
-            } else {
-                print("cant coordinate audio and video")
-            }
-        } catch let error {
-            print("Error attaching main video plater to coordination medium")
-            print(error)
-        }
-        
-        if let sidecarAudioUrl = sidecarAudioUrl {
-            print("creating sidecar audio av player")
-            let audioPlayer = AVPlayer(url: sidecarAudioUrl)
-            
-            do {
-                if #available(iOS 26.0, *) {
-                    print("coordinating video playback")
-                    try audioPlayer.playbackCoordinator.coordinate(using: coordinationMedium)
-                } else {
-                    print("cant coordinate audio and video")
-                }
-            } catch let error {
-                print("Error attaching main video plater to coordination medium")
-                print(error)
-            }
-        }
         
         let playerViewController = AVPlayerViewController()
         playerViewController.player = player
