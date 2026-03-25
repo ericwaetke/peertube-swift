eda963b8b365965043e3b6f839458d3fbbbe4ec2
Eric Wätke <email@ericwaetke.com>
2026-03-25 15:13:10 +0100

    feat: implement PeerTube login and authenticated feed synchronization

--- App/PeerTubeSwift/PeerTubeSwift/Tabs/SettingsTab.swift
+++ App/PeerTubeSwift/PeerTubeSwift/Tabs/SettingsTab.swift
@@ -19,40 +19,59 @@ struct SettingsTabFeature {
         
         @Shared(.inMemory("client")) var client: TubeSDKClient = try! TubeSDKClient(scheme: "https", host: "peertube.wtf")
         @Presents var editInstance: InstanceManagerFeature.State?
+        @Presents var login: LoginFeature.State?
+        var session: UserSession?
     }
     
     enum Action {
         case path(StackActionOf<SettingsPath>)
         
+        case onAppear
+        case sessionLoaded(UserSession?)
+        
         case editInstanceButtonTapped
         case editInstance(PresentationAction<InstanceManagerFeature.Action>)
         case goToCCVideo
         case setClient(TubeSDKClient)
+        
+        case loginButtonTapped
+        case login(PresentationAction<LoginFeature.Action>)
+        case logoutButtonTapped
     }
 
     @Reducer
     struct SettingsPath {
-        enum State {
-
-        }
-        enum Action {
-            
-        }
-        var body: some ReducerOf<Self> {
-            
-        }
+        enum State {}
+        enum Action {}
+        var body: some ReducerOf<Self> {}
     }
+    
+    @Dependency(\.authClient) var authClient
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
+            case .onAppear:
+                return .run { send in
+                    let session = try? await authClient.getSession()
+                    await send(.sessionLoaded(session))
+                }
+                
+            case let .sessionLoaded(session):
+                state.session = session
+                if let session = session {
+                    state.$client.withLock { $0.currentToken = session.token }
+                } else {
+                    state.$client.withLock { $0.currentToken = nil }
+                }
+                return .none
+                
             case .path(_):
                 return .none
             case .goToCCVideo:
                 return .none
             case .editInstanceButtonTapped:
                 guard let url = state.client.instance.urlComponents.url?.absoluteString else {
-                    print("Couldnt get current instance url")
                     return .none
                 }
                 state.editInstance = InstanceManagerFeature.State(instanceUrlString: url)
@@ -61,19 +80,11 @@ struct SettingsTabFeature {
                 switch delegate {
                 case let .saveNewInstance(url):
                     state.editInstance = nil
-                    
                     return .run { send in
-                        guard let host = url.host?.serialized else {
-                            print("Could not get host")
-                            return
-                        }
-                        
+                        guard let host = url.host?.serialized else { return }
                         do {
                             await send(.setClient(try TubeSDKClient(scheme: url.scheme, host: host)))
-                        } catch {
-                            print("Couldnt update client")
-                            print(error)
-                        }
+                        } catch {}
                     }
                 }
             case let .setClient(client):
@@ -81,10 +92,32 @@ struct SettingsTabFeature {
                 return .none
             case .editInstance:
                 return .none
+                
+            case .loginButtonTapped:
+                state.login = LoginFeature.State()
+                return .none
+                
+            case let .login(.presented(.delegate(.didLogin(session)))):
+                state.session = session
+                state.$client.withLock { $0.currentToken = session.token }
+                return .none
+                
+            case .login:
+                return .none
+                
+            case .logoutButtonTapped:
+                state.session = nil
+                state.$client.withLock { $0.currentToken = nil }
+                return .run { _ in
+                    try? await authClient.deleteSession()
+                }
             }
         }
         .ifLet(\.$editInstance, action: \.editInstance) {
             InstanceManagerFeature()
+        }
+        .ifLet(\.$login, action: \.login) {
+            LoginFeature()
         }
     }
 }
@@ -100,6 +133,20 @@ struct SettingsTab: View {
                         self.store.send(.editInstanceButtonTapped)
                     }
                 }
+                
+                Section("Account") {
+                    if let session = store.session {
+                        Text("Logged in as \(session.username)@\(session.host)")
+                        Button("Log Out", role: .destructive) {
+                            self.store.send(.logoutButtonTapped)
+                        }
+                    } else {
+                        Button("Log In to \(self.store.client.instance.host)") {
+                            self.store.send(.loginButtonTapped)
+                        }
+                    }
+                }
+                
                 Section("Debugging") {
                     Button("Go to Collective Change Video") {
                         self.store.send(.goToCCVideo)
@@ -107,6 +154,9 @@ struct SettingsTab: View {
                 }
             }
             .navigationTitle("Settings")
+            .task {
+                self.store.send(.onAppear)
+            }
             .sheet(item: $store.scope(state: \.editInstance, action: \.editInstance)) { store in
                 NavigationStack {
                     InstanceManager(store: store)
@@ -116,7 +166,6 @@ struct SettingsTab: View {
                             ToolbarItem {
                                 Button("Save") {
                                     guard let url = store.state.instanceUrl else { return }
-                                    
                                     store.send(.delegate(.saveNewInstance(url: url)))
                                 }
                                 .disabled(!store.state.readyToSaveInstance)
@@ -124,10 +173,14 @@ struct SettingsTab: View {
                         }
                 }
             }
+            .sheet(item: $store.scope(state: \.login, action: \.login)) { loginStore in
+                NavigationStack {
+                    LoginView(store: loginStore)
+                }
+            }
         }
     }
 }
-
 
 #Preview {
     let _ = prepareDependencies {
