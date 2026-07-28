31f6f29d2929baeb491d820d64355c359b50831e
Eric Wätke <email@ericwaetke.com>
2026-07-28 11:15:16 +0200

    feat: added button styles stub

---
+++ PeerTubeSwift/Helpers/ButtonStyles.swift
@@ -0,0 +1,37 @@
+//
+//  ButtonStyles.swift
+//  PeerTubeSwift
+//
+//  Created by Eric Wätke on 28.07.26.
+//
+
+import SwiftUI
+
+//struct RiverTinted: ButtonStyle {
+//    func makeBody(configuration: Configuration) -> some View {
+//        configuration.label
+//            .padding()
+//            .background(Color(red: 0, green: 0, blue: 0.5))
+//            .foregroundStyle(.white)
+//            .clipShape(Capsule())
+//    }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        Button("Press Me") {
+//            print("Button pressed!")
+//        }
+//        .buttonStyle(BlueButton())
+//    }
+//}
+//
+//#Preview {
+//
+//    return VStack {
+//        Button("Press Me") {
+//                    print("Button pressed!")
+//                }
+//                .buttonStyle(RiverTinted())
+//    }
+//}
