500ae7e3769b4276e5b600db544dcc381c59924f
Eric Wätke <email@ericwaetke.com>
2026-07-30 14:31:47 +0200

    feat: added new semantic colors

--- PeerTubeSwift/Helpers/Colors.swift
+++ PeerTubeSwift/Helpers/Colors.swift
@@ -12,4 +12,9 @@ extension Color {
   static let labelSecondary = Color("Blackish/900-70")
   static let labelAction = Color("Blue/900")
   static let labelHighlight = Color("Blue/700")
+  static let labelDesctructive = Color("Red/900")
+
+  static let fillPrimary = Color("Blue/700")
+  static let fillSecondary = Color("Blue/100")
+  static let fillDestructive = Color("Red/100")
 }
