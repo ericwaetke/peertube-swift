557e53c566a849476c2a0fa3bbe2e01d47b5a0fd
Eric Wätke <email@ericwaetke.com>
2026-08-03 16:32:35 +0200

    feat: Account Settings Subs

--- PeerTubeSwift/Views/Settings.swift
+++ PeerTubeSwift/Views/Settings.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import FontKit
 import SwiftUI
 import TubeSDK
 
@@ -13,19 +14,18 @@ import TubeSDK
 public struct SettingsFeature {
   @ObservableState
   public struct State: Equatable {
-      let text: String
+    let text: String
   }
 
   public enum Action {
-      case buttonTapped
+    case buttonTapped
   }
-
 
   public var body: some Reducer<State, Action> {
     Reduce<State, Action> { state, action in
       switch action {
       case .buttonTapped:
-          return .none
+        return .none
       }
     }
   }
@@ -36,8 +36,103 @@ public struct SettingsView: View {
 
   public var body: some View {
     Form {
-      
+      Section {
+        Button {
+        } label: {
+          Text("peertube.wtf")
+            .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+            .foregroundStyle(Color(uiColor: .label))
+        }
+      } header: {
+        HStack {
+          Text("Your Community")
+            .font(
+              CustomFont.inclusiveSansSemiBold.swiftUIFont(size: 13, relativeTo: .footnote)
+            )
+            .textCase(.uppercase)
+            .foregroundStyle(Color.labelSecondary)
+          Spacer()
+        }
+      }
+      Section {
+        Button {
+        } label: {
+          Text("Display Name")
+            .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+            .foregroundStyle(Color(uiColor: .label))
+        }
+        Button {
+        } label: {
+          Text("Email")
+            .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+            .foregroundStyle(Color(uiColor: .label))
+        }
+        Button {
+        } label: {
+          HStack {
+            Text("Password")
+              .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+              .foregroundStyle(Color(uiColor: .label))
+            Spacer()
+            Text("Change")
+              .foregroundStyle(Color.labelAction)
+              .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+          }
+        }
+      } header: {
+        HStack {
+          Text("Account")
+            .font(
+              CustomFont.inclusiveSansSemiBold.swiftUIFont(size: 13, relativeTo: .footnote)
+            )
+            .textCase(.uppercase)
+            .foregroundStyle(Color.labelSecondary)
+          Spacer()
+        }
+      }
+      Section {
+        Button {
+        } label: {
+          Text("Languages")
+            .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+            .foregroundStyle(Color(uiColor: .label))
+        }
+        Button {
+        } label: {
+          Text("Prefered Categories")
+            .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+            .foregroundStyle(Color(uiColor: .label))
+        }
+      } header: {
+        HStack {
+          Text("Recommendations")
+            .font(
+              CustomFont.inclusiveSansSemiBold.swiftUIFont(size: 13, relativeTo: .footnote)
+            )
+            .textCase(.uppercase)
+            .foregroundStyle(Color.labelSecondary)
+          Spacer()
+        }
+      }
+      Button {
+      } label: {
+        Text("Log out")
+          .foregroundStyle(Color.labelAction)
+          .font(CustomFont.inclusiveSansRegular.swiftUIFont(size: 17, relativeTo: .body))
+          .containerRelativeFrame(.horizontal)
+      }
     }
-    .navigationTitle("Settings")
+    .navigationTitle("Account Settings")
+  }
+}
+
+#Preview {
+  NavigationStack {
+    SettingsView(
+      store: Store(
+        initialState: SettingsFeature.State(text: "some")
+      ) {
+        SettingsFeature()
+      })
   }
 }
